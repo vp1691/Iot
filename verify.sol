17d77cd73b58c632a06f244ccab590cc96ee7361
Vivek Prasad <69196747+vp1691@users.noreply.github.com>
2023-01-03 02:05:39 +0530

    Update verify.sol

--- verify.sol
+++ verify.sol
@@ -51,3 +51,15 @@ contract verifySign{
     }
 
 }
+
+/*hash = "0xcf36ac4f97dc10d91fc2cbb20d718e94a8cbfe0f82eaedc6a4aa38946fb797cd" 
+Sign message hash 
+    Signature will be :: 0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c 146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b 
+
+Verify signature 
+signer = 0xB273216C05A8c0D4F0a4Dd0d7Bae1D2EfFE636dd 
+to = 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C 
+amount = 123 
+message = "coffee and donuts" 
+nonce = 1 
+signature =   0x993dab3dd91f5c6dc28e17439be475478f5635c92a56e17e82349d3fb2f166196f466c0b4e0c 146f285204f0dcb13e5ae67bc33f4b888ec32dfe0a063e8f3f781b*/
